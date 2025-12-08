a7b5e8c433079a50ed13f18271876d1a503166cc
Deepti Agrawal <deepti.agl2912@gmail.com>
2025-12-08 18:16:39 +0530

    correcting issues

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -4,6 +4,7 @@ import Footer from "../../components/footer/Footer";
 import Educations from "../../containers/education/Educations";
 import Certifications from "../../containers/certifications/Certifications";
 // import EducationImg from "./EducationImg"; //Old code
+import eduImage from "E:/deept-agl.github.io/src/assets/images/deep_img.png";
 import eduImage from "E:/deept-agl.github.io/src/assets/images/deep_img.png"; //New Image Added
 import "./EducationComponent.css";
 import { Fade } from "react-reveal";
